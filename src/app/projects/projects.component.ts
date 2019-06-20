959358fcecb7232ef947d8bd57673c7bf87ecb25
brian hensley and mathew akre <brian+mathew@>
2019-06-20 16:28:05 -0700

    add/remove comments

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,19 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import { FirebaseListObservable } from 'angularfire2/database';
-import { ProjectService } from 'app/project.service';
+import { FirebaseListObservable } from 'angularfire2/database'; //add this for firebase
+import { ProjectService } from 'app/project.service'; //add this for firebase
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.css'],
-  providers: [ProjectService]
+  providers: [ProjectService] //add this for firebase
 })
 export class ProjectsComponent implements OnInit {
-  projects: FirebaseListObservable<any[]>;
-  constructor(private service: ProjectService) { 
-    this.projects = service.getProjects();
+  projects: FirebaseListObservable<any[]>; //add this for firebase
+  constructor(private service: ProjectService) {   //add this for firebase
+    this.projects = service.getProjects(); //add this for firebase
 
-    this.projects.subscribe( items => {
+    this.projects.subscribe( items => { //add this for firebase
       console.log(items);
       
     });
